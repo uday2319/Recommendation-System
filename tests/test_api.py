72988b11f391fde25a911338f72ffcaf27beaac7
uday2319 <udaydk587@gmail.com>
2025-12-19 12:42:05 +0530

    debugged test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,11 +3,13 @@ from api.app2 import app
 client=TestClient(app)
 def health_test_check():
     response=client.grt("/")
-    assert response.statuscode == 200
+    assert response.status_code == 200
 
 def test_recommendation_endpoint():
     response=client.get("/recommend/1?top_k=5")
-    assert response.statuscode == 200
+    assert response.status_code == 200
     data=response.json()
-    assert isinstance(data,list)
-    assert len(data)==5
+    assert "recommendations" in data 
+    assert isinstance(data["recommendations"], list)
+    assert len(data["recommendations"])==5
+
